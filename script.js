5cfca784f7ec008c18a60fc573fecb8f11148fdb
Atulgupta0403 <atulgupta0403@gmail.com>
2024-02-09 18:25:42 +0530

    new index

---
+++ script.js
@@ -0,0 +1,10 @@
+// let btn=document.querySelector(".panelall");
+let btn1=document.querySelector("button");
+let hd=document.querySelector(".hide");
+// btn.addEventListener("click",()=>{
+//     console.log("clicked");
+// })
+btn1.addEventListener("click",()=>{
+    hd.disabled=false;
+    console.log("clicked");
+})
